0bce77496c4a6a08e1b7c601dbce9630c06ac535
Diego Rojas <diego.rojas@equifax.com>
2020-07-03 19:56:49 -0300

    v. con el analisis de datos

---
+++ data_analysis_jobs.py
@@ -0,0 +1,132 @@
+"""
+OBJETIVO: 
+    - Calcular el porcentaje de apariciones de los lenguages de programacion y bibliotecas en las solicitudes de empleo para la posición de Data Scientist.
+CREADO POR: Diego Rojas
+ULTIMA VEZ EDITADO: Julio 2020
+"""
+
+import pandas as pd
+import numpy as np
+from pymongo import MongoClient
+from matplotlib import pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+# Seleccionamos la base de datos y la coleccion en Mongodb
+client = MongoClient('localhost')
+db = client['indeed']
+col = db['data_scientist_jobs']
+
+df = pd.DataFrame.from_dict(col.find({},{'_id':0, 'title':1, 'item1':1, 'item2':1, 'item3':1}))
+
+# Creamos una varibale que concatene todas las descripciones
+df['all_item'] = df['item1'].astype(str) + ' ' + df['item2'].astype(str) + ' ' + df['item3'].astype(str)
+df_all_item = pd.DataFrame(df['all_item'])
+
+# Eliminados duplicados de posiciones que se repiten en varias paginas
+df_all_item = df_all_item.drop_duplicates()
+
+# Separamos cada una de las palabras en un nuevo dataframe
+s = df_all_item.all_item.str.split(expand = True).stack().str.capitalize()
+s.index = s.index.droplevel(-1)
+s.name = 'all_item_2'
+df_split = pd.DataFrame(s)
+
+# Lo unimos al dataframe sin duplicados
+df_split_all_item = df_all_item.join(df_split)
+
+# Volvemos a eliminar duplicados. Esto lo hacemos para evitar que se repita un lenguage o biblioteca en la misma publicación 
+df_split_all_item = df_split_all_item.drop_duplicates()
+
+# Agrupamos para ver cuantas veces se repita cada palabra
+df_grouped = df_split_all_item.groupby(['all_item_2']).size().reset_index(name = 'count').sort_values(by = 'count', ascending = False)
+
+# Creamos una funcion para que corregir o homogeneizar algunas palabras
+def correciones(texto):
+    texto_corregido = (
+        texto.replace('Sci-kit','Scikit-learn').replace('Sklearn','Scikit-learn')
+        .replace('Slq','Sql').replace('Mysql','Sql').replace('Postgresql','Sql')
+        .replace('Ggplot2','Ggplot').replace('Non-sql','Nosql').replace("Python's",'Python')
+        .str.strip()
+        )                    
+    return texto_corregido
+
+df_grouped['all_item_2'] = correciones(df_grouped['all_item_2'])
+df_grouped = df_grouped.rename(columns = {"all_item_2": "Lenguages_all"})
+
+# Realizamos el análisis para los lenguajes de programacion
+
+# Filtramos por los lenguajes
+filter_lenguages = ['Python','Sql','R','Spark','Hadoop','Java','C++','Sas','Hive','Matlab','Scala','Nosql','C','Pig','Go','Stata','C#','Weka','Cassandra','Mongodb','Impala','Ruby','Julia']
+df_lenguajes = df_grouped[df_grouped.Lenguages_all.isin(filter_lenguages)]
+# Contamos la cantidad de palabras
+df_lenguages_grouped = pd.DataFrame(df_lenguajes.groupby('Lenguages_all')['count'].sum())
+# Calculamos el % que aparece cada palabra dentro del total de lenguajes
+df_lenguages_grouped['total'] = df_lenguages_grouped['count'].sum(axis = 0)
+df_lenguages_grouped['percentage'] = round((df_lenguages_grouped['count']/df_lenguages_grouped['total'])*100,2)
+df_lenguages_grouped = pd.DataFrame(df_lenguages_grouped).reset_index()
+# Nos quedamos con los que tienen una participación mayor al 3%
+df_lenguages_grouped['Lenguages'] = np.where(df_lenguages_grouped.percentage<3.0, 'Others', df_lenguages_grouped.Lenguages_all)
+df_lenguages_grouped = pd.DataFrame(df_lenguages_grouped.groupby('Lenguages')['percentage'].sum()).reset_index().sort_values(by = 'percentage', ascending = False)
+
+# Realizamos el analisis para las bibliotecas 
+
+# Filtramos por las bibliotecas
+filter_Libraries = ['Tensorflow','Scikit-learn','Pandas','Numpy','Pytorch','Keras','Pyspark','Matplotlib','Seaborn','Docker','Flask','Ggplot','Shiny','H2o','PowerBI','Qlikview','Django','Dplyr','Elasticsearch','Qliksense','Cognos','Spotfire']
+df_bibliotecas = df_grouped[df_grouped.Lenguages_all.isin(filter_Libraries)].rename(columns = {"Lenguages_all": "Libraries_all"})
+# Contamos la cantidad de palabras
+df_Libraries_grouped = pd.DataFrame(df_bibliotecas.groupby('Libraries_all')['count'].sum()).reset_index()
+# Calculamos el % que aparece cada palabra dentro del total de bibliotecas
+df_Libraries_grouped['total'] = df_Libraries_grouped['count'].sum(axis = 0)
+df_Libraries_grouped['percentage'] = round((df_Libraries_grouped['count']/df_Libraries_grouped['total'])*100,2)
+# Nos quedamos con los que tienen una participación mayor al 4%
+df_Libraries_grouped['Libraries'] = np.where(df_Libraries_grouped.percentage<4.0, 'Others', df_Libraries_grouped.Libraries_all)
+df_Libraries_grouped = pd.DataFrame(df_Libraries_grouped.groupby('Libraries')['percentage'].sum()).reset_index().sort_values(by = 'percentage', ascending = False)
+
+# Ahora realizamos dos gráfico tipo torta
+
+fig = plt.figure(1, figsize = (15,10))
+the_grid = GridSpec(2, 2)
+
+# Gráfico para Programming Lenguages
+ax = plt.subplot(the_grid[0, 0], aspect = 1, title = 'Programming Lenguages')
+
+data = df_lenguages_grouped.percentage
+ingredients = df_lenguages_grouped.Lenguages
+
+def func(pct, allvals):
+    absolute = int(pct/100.*np.sum(allvals))
+    return "{:.1f}%".format(pct, absolute)
+
+wedges, texts, autotexts = ax.pie(data, autopct = lambda pct:func(pct, data),
+                                    textprops = dict(color = "w"))
+
+ax.legend(wedges, ingredients,
+        loc = "center left",
+        bbox_to_anchor = (1, 0, 0.5, 1),
+        fontsize = 13)
+
+plt.setp(autotexts, size = 12, weight = "bold")
+
+# Gráfico para las Libraries
+ax = plt.subplot(the_grid[0, 1], aspect = 1, title = 'Libraries')
+
+data = df_Libraries_grouped.percentage
+ingredients = df_Libraries_grouped.Libraries
+
+def func(pct, allvals):
+    absolute = int(pct/100.*np.sum(allvals))
+    return "{:.1f}%".format(pct, absolute)
+
+wedges, texts, autotexts = ax.pie(data, autopct = lambda pct: func(pct, data),
+                                    textprops = dict(color = "w"))
+
+ax.legend(wedges, ingredients,
+            loc = "center left",
+            bbox_to_anchor = (1, 0, 0.5, 1),
+            fontsize = 13)
+
+plt.setp(autotexts, size = 12, weight = "bold")
+
+plt.suptitle("% Programming Skills Required for Data Scientist Jobs", fontsize = 16, fontweight = 'bold')
+
+plt.show()
---
+++ scraping_data_scientist_jobs.py
@@ -0,0 +1,112 @@
+"""
+Objetivo: Extraer la descripcion de cargo de los puestos de Data Scientist en USA publicados en la pagina web de emples Indeed.
+Elaborado por: Diego Rojas
+Fecha: 1 Julio 2020
+"""
+from datetime import date
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from pymongo import MongoClient
+
+#Creamos una colección en Mongodb donde vamos a guardar los datos de los libros
+client = MongoClient('localhost')
+db = client['indeed']
+col = db['data_scientist_jobs']
+    
+# Definimos el User Agent
+opts = Options()
+opts.add_argument("user-agent = Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36")
+
+url_todas = ['https://www.indeed.com/jobs?q = data+scientist&l = United+States&fromage = last']
+for i in range (10, 310, 10):
+    url = 'https://www.indeed.com/jobs?q = data+scientist&l = United+States&fromage = last&start = ' + str(i)
+    url_todas.append(url)
+
+#Antes de continuar, creamos una función que elimine la tabulación y saltos de linea
+def limpieza(texto):
+    texto_corregido = (
+        texto.replace('\n', ' ').replace('\t', ' ').replace(',',' ')
+        .replace('.',' ').replace('(',' ').replace(')',' ').replace(':',' ')
+        .replace('/',' ').replace(';',' ')
+        )
+    return texto_corregido
+
+# Ejecutar el proceso hasta que llegue a la ultima url
+PAGINA = 1
+
+#Recorremos cada uno de los urls
+for url in url_todas:
+    
+    driver = webdriver.Chrome(executable_path = r"C:\Users\dar12\Downloads\Web Scraping\chromedriver.exe", chrome_options = opts)
+    driver.get(url)
+
+    # Mientras la PAGINA en la que me encuentre, sea menor que la maxima PAGINA que voy a sacar... sigo ejecutando...
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//div[@class = "jobsearch-SerpJobCard unifiedRow row result clickcard"]//h2//a[@target = "_blank"]')))
+
+    links_productos = WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//div[@class = "jobsearch-SerpJobCard unifiedRow row result clickcard"]//h2//a[@target = "_blank"]')))
+
+    links_de_la_PAGINA = []
+    id = 1
+    for a_link in links_productos:
+        links_de_la_PAGINA.append(a_link.get_attribute("href"))
+    print('PAGINA ',str(PAGINA),' iniciada') 
+
+    for link in links_de_la_PAGINA:
+
+        try:
+        # Voy a cada uno de los links de los detalles de los productos
+            driver.get(link)
+
+            try:
+                title = driver.find_element(By.XPATH, '//div[@class = "jobsearch-JobInfoHeader-title-container"]//h3').text
+                title = limpieza(title)
+            except:
+                title = 'N.D'
+            try:
+                item1 = driver.find_element(By.XPATH, '//div[@id = "jobDescriptionText"]//ul[1]').text
+                item1 = limpieza(item1)
+            except:
+                item1 = 'N.D' 
+            try:
+                item2 = driver.find_element(By.XPATH, '//div[@id = "jobDescriptionText"]//ul[2]').text
+                item2 = limpieza(item2)
+            except:
+                item2 = 'N.D'
+            try:
+                item3 = driver.find_element(By.XPATH, '//div[@id = "jobDescriptionText"]//ul[3]').text
+                item3 = limpieza(item3)
+            except:
+                item3 = 'N.D'             
+
+            #Agregamos una variable con la fecha en que estamos haciendo la extracción
+            fecha_subida = str(date.today())
+        
+            #Inserto los datos en la colección de Mongodb
+            col.insert_one({
+                'title': title,
+                'item1': item1,
+                'item2': item2,
+                'item3': item3,
+                'fecha_subida':fecha_subida
+            })
+            id+ = 1
+            
+            # Piso el boton de retroceso
+            driver.back()
+        
+        except Exception as e:
+            print (e)
+            # Si sucede algún error regreso y sigo con el siguiente job.
+            driver.back()
+
+    print('PAGINA ',str(PAGINA),' finalizada')    
+    PAGINA+ = 1
+    print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+    driver.close()
+ 
